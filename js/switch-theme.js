f1159f6b5028877575c742b026e6af295c1862a5
Anthony <anthony.tron@etu.univ-amu.fr>
2019-12-05 10:59:40 +0100

    top bar menu, theme button...
    + The top bar menu is now more responsive
    + The button to switch between dark mode and light mode is now integrated
    + The website now remembers what theme you've chosen

--- js/switch-theme.js
+++ js/switch-theme.js
@@ -1,6 +1,23 @@
 var body = document.querySelector('body')
-var switchButton = document.querySelector('.swich-theme')
+var switchButton = document.querySelector('.switch-theme')
+var theme = localStorage.getItem("theme")
+
+if (theme === 'theme-b') {
+    body.classList.toggle('theme-b')
+    switchButton.innerHTML = 'Dark theme'
+} else {
+    switchButton.innerHTML = 'Light theme'
+}
 
 switchButton.addEventListener('click', function(event) {
     body.classList.toggle('theme-b')
+
+    if (body.classList.contains('theme-b')) {
+        switchButton.innerHTML = 'Dark theme'
+        localStorage.setItem('theme', 'theme-b')
+    }
+    else  {
+        switchButton.innerHTML = 'Light theme'
+        localStorage.setItem('theme', '')
+    }
 });
